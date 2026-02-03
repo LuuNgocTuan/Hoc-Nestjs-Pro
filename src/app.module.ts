07428d44cf55d5bd0725c4d188efc3dddfc89b34
NgocTuan <ngoctuanluu@outlook.com>
2026-02-03 23:03:15 +0700

    Test kết nối DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://hoiNgocTuan:CpY4Ue0ah5PMWOIX@cluster0.qseat.mongodb.net/')],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
