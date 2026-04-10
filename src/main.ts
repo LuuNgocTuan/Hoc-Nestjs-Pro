6b117522debd74e9079b78ca9c570c4fa645fdba
NgocTuan <ngoctuanluu@outlook.com>
2026-04-10 22:14:34 +0700

    Bài học 85 #52.2 Setup CORS

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ dns.setServers(['1.1.1.1', '8.8.8.8']);
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     const reflector = app.get(Reflector);
-    app.useGlobalGuards(new JwtAuthGuard(reflector));
+    // app.useGlobalGuards(new JwtAuthGuard(reflector));
 
     app.useStaticAssets(join(__dirname, '..', 'public'));//static files like css, img
     app.setBaseViewsDir(join(__dirname, '..', 'views')); //views
@@ -24,6 +24,15 @@ async function bootstrap() {
 
     const configService = app.get(ConfigService);
     const port = configService.get('PORT');
+
+    app.enableCors(
+        {
+            "origin": "http://localhost:3000",
+            "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+            "preflightContinue": false,
+            "optionsSuccessStatus": 204
+        }
+    );
     await app.listen(port);
 
 }
