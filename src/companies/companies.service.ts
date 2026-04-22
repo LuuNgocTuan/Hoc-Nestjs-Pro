15c1c18e5cb2fcab804eabe72d4104e8fd977c81
NgocTuan <ngoctuanluu@outlook.com>
2026-04-22 20:49:41 +0700

    #54 Bài tập Create Company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Company } from './schemas/company.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CompaniesService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
-  }
+    constructor(@InjectModel(Company.name) private CompanyModel: Model<Company>) { }
+    async create(createCompanyDto: CreateCompanyDto) {
+        const company = await this.CompanyModel.create({ ...createCompanyDto });
+        return company;
 
-  findAll() {
-    return `This action returns all companies`;
-  }
+        // return 'This action adds a new company';
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} company`;
-  }
+    findAll() {
+        return `This action returns all companies`;
+    }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} company`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
-  }
+    update(id: number, updateCompanyDto: UpdateCompanyDto) {
+        return `This action updates a #${id} company`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} company`;
+    }
 }
