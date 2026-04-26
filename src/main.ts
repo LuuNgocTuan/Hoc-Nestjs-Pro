561b8ec863fe6ebadc691f77eadad80827dc0b0a
NgocTuan <ngoctuanluu@outlook.com>
2026-04-26 18:40:35 +0700

    #62 Verion APIs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import *as dns from 'dns';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -36,6 +36,14 @@ async function bootstrap() {
             "optionsSuccessStatus": 204
         }
     );
+
+    //config versioning cho API, khi mình enable versioning thì mình sẽ phải thêm version vào đường dẫn của API, ví dụ như /api/v1/companies, /api/v2/companies,... để phân biệt các version của API. Khi mình gọi API thì mình sẽ phải gọi đúng version của API đó, nếu không thì sẽ bị lỗi 404 Not Found. Việc này giúp cho việc quản lý và phát triển API trở nên dễ dàng hơn, khi mình muốn thay đổi hoặc thêm mới một tính năng nào đó thì mình chỉ cần tạo một version mới của API mà không ảnh hưởng đến các version cũ đã tồn tại.
+    app.setGlobalPrefix('api');
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: ['1', '2']
+    });
+
     await app.listen(port);
 
 }
